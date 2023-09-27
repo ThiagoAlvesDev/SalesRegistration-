120b0faf0befd20c5e205fc33c0f12d4c3ece7d8
Thiago Alves <thiagosales06@hotmail.com>
2023-09-26 21:08:52 -0300

    SellerService and indexSeller

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -30,6 +31,8 @@ namespace SalesWebMVC
             services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(stringConection, ServerVersion.AutoDetect(stringConection)));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
