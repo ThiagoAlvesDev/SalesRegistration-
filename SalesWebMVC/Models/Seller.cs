e1866e49cd10ff40f0ac28f8c4585e137e7aa320
Thiago Alves <thiagosales06@hotmail.com>
2023-09-26 22:16:08 -0300

    Add foreingkey

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -11,6 +11,7 @@ namespace SalesWebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecords> Sales { get; set; } = new List<SalesRecords>();
 
         public Seller() { }
